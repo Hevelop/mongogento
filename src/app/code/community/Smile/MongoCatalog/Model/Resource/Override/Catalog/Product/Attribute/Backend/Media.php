fdee38f4df3839c6cd321fb039fdc89f56fd2646
Jared Chmielecki <jchmielecki@stanleygibbons.com>
2016-04-04 22:12:34 -0400

    missed one

--- src/app/code/community/Smile/MongoCatalog/Model/Resource/Override/Catalog/Product/Attribute/Backend/Media.php
+++ src/app/code/community/Smile/MongoCatalog/Model/Resource/Override/Catalog/Product/Attribute/Backend/Media.php
@@ -99,7 +99,7 @@ class Smile_MongoCatalog_Model_Resource_Override_Catalog_Product_Attribute_Backe
         $updateFilter = array('_id' => $product->getId());
         $updateValue = array('galleries.' . $attributeCode => array_values($savedGallery));
 
-        $this->_getDocumentCollection()->update($updateFilter, array('$set' => $updateValue));
+        $this->_getDocumentCollection()->updateMany($updateFilter, array('$set' => $updateValue));
 
         return $this;
     }
